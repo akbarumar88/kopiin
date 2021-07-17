20d2cace88c2e9d38c8564119ae301f04b74c340
Akbar Umar Alfaroq <akbarumar88@gmail.com>
2021-07-17 14:57:55 +0700

    Selesai desain toko & barang

--- src/components/dashboard/Home.js
+++ src/components/dashboard/Home.js
@@ -1,7 +1,13 @@
-import * as React from 'react';
+import React, {Component} from 'react';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
-import {Alert, ToastAndroid} from 'react-native';
+import {
+  Alert,
+  ToastAndroid,
+  Dimensions,
+  RefreshControl,
+  NativeAppEventEmitter,
+} from 'react-native';
 import {
   NativeBaseProvider,
   Box,
@@ -19,6 +25,8 @@ import {
   ScrollView,
   useToast,
   Pressable,
+  Image,
+  Center,
 } from 'native-base';
 import {BASE_URL, OAUTH_CLIENT_ID, theme} from '../../utilitas/Config';
 import {
@@ -34,27 +42,60 @@ import Loading from '../universal/Loading';
 import {errMsg} from '../../utilitas/Function';
 import AsyncStorage from '@react-native-community/async-storage';
 import Resource from '../universal/Resource';
-import {MerchantShimmer} from '../universal/Placeholder';
+import {MerchantShimmer, BarangShimmer} from '../universal/Placeholder';
 
 export default class Home extends Component {
+  defaultStoreAvatar =
+    'https://cdn.icon-icons.com/icons2/1706/PNG/512/3986701-online-shop-store-store-icon_112278.png';
+  defaultProductAvatar = "https://cdn.iconscout.com/icon/free/png-512/box-with-stuff-2349406-1955397.png"
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      refresh: 1,
+    };
+  }
+
   render() {
     return (
       <NativeBaseProvider>
-        <ScrollView>
-          <Text bold>Toko / Kedai Terdekat</Text>
-          {this.listToko()}
-          <Text bold>Mungkin anda butuh barang ini</Text>
-          {this.listBarang()}
-        </ScrollView>
+        <Box bgColor={'#ff0000'} flex={1}>
+          {/* <Box>
+            <Text>Ini Header Gan</Text>
+          </Box> */}
+          <ScrollView
+            paddingX={4}
+            paddingY={4}
+            backgroundColor={'#fff'}
+            nestedScrollEnabled
+            refreshControl={
+              <RefreshControl
+                refreshing={false}
+                onRefresh={() => {
+                  this.setState(s => ({refresh: s.refresh + 1}));
+                }}
+              />
+            }>
+            <Box>
+              <Text bold>Toko / Kedai Terdekat</Text>
+              {this.listToko()}
+              <Text bold>Mungkin anda butuh barang ini</Text>
+              {this.listBarang()}
+            </Box>
+          </ScrollView>
+        </Box>
       </NativeBaseProvider>
     );
   }
 
   listToko = () => {
     return (
-      <Resource url={`${BASE_URL()}/dashboard/shop`}>
+      <Resource
+        url={`${BASE_URL()}/dashboard/shop`}
+        params={{refresh: this.state.refresh}}>
         {({loading, error, payload: data, refetch}) => {
-          if (loading) {
+          if (loading ) {
             return <MerchantShimmer />;
           } else if (error) {
             return <Text>{errMsg('Load Merchant')}</Text>;
@@ -66,52 +107,65 @@ export default class Home extends Component {
             <ScrollView
               horizontal={true}
               showsHorizontalScrollIndicator={false}>
-              <View style={{flexDirection: 'row'}}>
-                {data.data.map((item, index) => {
-                  return (
-                    <Pressable
-                      key={index}
-                      onPress={() => {
-                        if (index % 2 == 0) {
-                          this.props.navigation.navigate('ApotekDetail', {
-                            appid,
-                            kl_nama: nama,
-                          });
-                        } else {
-                          this.props.navigation.navigate('KlinikDetail', {
-                            data: item,
-                          });
-                        }
-                      }}>
-                      {/* <View style={[{width}, style.wCardApotek, marginLeft]}> */}
-                        <RNImage
+              <HStack my={4}>
+                {data.data.map(
+                  (
+                    {
+                      nama_toko,
+                      alamat_toko,
+                      jenis,
+                      foto = this.defaultStoreAvatar,
+                      jarak = 0,
+                    },
+                    index,
+                  ) => {
+                    let imgWidth = Dimensions.get('window').width / 7;
+                    let itemWidth = Dimensions.get('window').width / 3.5;
+                    return (
+                      <Pressable
+                        alignItems="flex-start"
+                        bgColor="coolGray.100"
+                        _pressed={{backgroundColor: 'coolGray.300'}}
+                        p={2}
+                        mr={2}
+                        key={index}
+                        w={itemWidth}
+                        borderRadius={8}
+                        onPress={() => {}}>
+                        {/* <View style={[{width}, style.wCardApotek, marginLeft]}> */}
+                        <Image
+                          alignSelf="center"
                           resizeMode="contain"
-                          style={[{width, height: width}, style.imgApotek]}
+                          style={[
+                            {width: imgWidth, height: imgWidth},
+                            {
+                              borderTopLeftRadius: 10,
+                              borderTopRightRadius: 10,
+                            },
+                          ]}
                           source={{
                             uri: foto,
                           }}
+                          alt={nama_toko}
                         />
-                        <View style={{padding: 10}}>
-                          <Text numberOfLines={2} style={{fontSize: 12}}>
-                            {nama}
+
+                        <Box px={0}>
+                          <Text fontSize="sm" isTruncated>
+                            {nama_toko}
                           </Text>
-                          <View style={{flexDirection: 'row'}}>
-                            <Text
-                              numberOfLines={3}
-                              style={{
-                                fontSize: 10,
-                                color: 'gray',
-                                marginLeft: 0,
-                              }}>
-                              {kota ? kota : ''}
-                            </Text>
-                          </View>
-                        </View>
-                      {/* </View> */}
-                    </Pressable>
-                  );
-                })}
-              </View>
+                          <Text fontSize="xs" bold color="grey">
+                            {alamat_toko}
+                          </Text>
+                          <Text fontSize="xs" color="grey">
+                            {jarak} KM dari lokasi anda.
+                          </Text>
+                        </Box>
+                        {/* </View> */}
+                      </Pressable>
+                    );
+                  },
+                )}
+              </HStack>
             </ScrollView>
           );
         }}
@@ -119,5 +173,87 @@ export default class Home extends Component {
     );
   };
 
-  listBarang = () => {};
+  listBarang = () => {
+    return (
+      <Resource
+        url={`${BASE_URL()}/dashboard/product`}
+        params={{refresh: this.state.refresh}}>
+        {({loading, error, payload: data, refetch}) => {
+          if (loading) {
+            return <BarangShimmer />;
+          } else if (error) {
+            return <Text>{errMsg('Load Barang')}</Text>;
+          } else if (!data.status) {
+            return <Text>{errMsg('Load Barang')}</Text>;
+          }
+
+          return (
+            <HStack my={4} flexWrap="wrap">
+              {data.data.map(
+                (
+                  {
+                    nama,
+                    deskripsi,
+                    harga,
+                    nama_toko,
+                    alamat_toko,
+                    jenis,
+                    foto = this.defaultProductAvatar,
+                    jarak,
+                  },
+                  index,
+                ) => {
+                  let imgWidth = Dimensions.get('window').width / 7;
+                  let itemWidth = Dimensions.get('window').width / 3.5;
+                  return (
+                    <Pressable
+                      alignItems="flex-start"
+                      bgColor="coolGray.100"
+                      _pressed={{backgroundColor: 'coolGray.300'}}
+                      p={2}
+                      mr={2}
+                      mb={2}
+                      key={index}
+                      w={itemWidth}
+                      borderRadius={8}
+                      onPress={() => {}}>
+                      {/* <View style={[{width}, style.wCardApotek, marginLeft]}> */}
+                      <Image
+                        alignSelf="center"
+                        resizeMode="contain"
+                        style={[
+                          {width: imgWidth, height: imgWidth},
+                          {
+                            borderTopLeftRadius: 10,
+                            borderTopRightRadius: 10,
+                          },
+                        ]}
+                        source={{
+                          uri: foto,
+                        }}
+                        alt={nama}
+                      />
+
+                      <Box px={0}>
+                        <Text fontSize="sm" isTruncated>
+                          {nama}
+                        </Text>
+                        <Text fontSize="xs" bold color="grey">
+                          {deskripsi}
+                        </Text>
+                        <Text fontSize="xs" color="grey">
+                          {harga}
+                        </Text>
+                      </Box>
+                      {/* </View> */}
+                    </Pressable>
+                  );
+                },
+              )}
+            </HStack>
+          );
+        }}
+      </Resource>
+    );
+  };
 }
--- src/components/universal/Placeholder.js
+++ src/components/universal/Placeholder.js
@@ -6,6 +6,8 @@ import ContentLoader, {
 import {Dimensions, FlatList, View} from 'react-native';
 import React, {Component} from 'react';
 
+let windowWidth = Dimensions.get("window").width;
+
 export const MerchantShimmer = props => {
   const {portrait} = props;
 
@@ -23,7 +25,40 @@ export const MerchantShimmer = props => {
         return `${index}`;
       }}
       style={{}}
-      contentContainerStyle={{paddingHorizontal: 20}}
+      contentContainerStyle={{}}
+      renderItem={({item, index}) => {
+        return (
+          <ContentLoader height={itemHeight} width={itemWidth}>
+            <Rect x="0" y="20" width={pictureWidth} height={pictureHeight} />
+            <Rect x="0" y={pictureHeight + 5 * 1 + 20} width="40" height="5" />
+            <Rect x="0" y={pictureHeight + 5 * 3 + 20} width="35" height="5" />
+            <Rect x="0" y={pictureHeight + 5 * 5 + 20} width="40" height="5" />
+            <Rect x="0" y={pictureHeight + 5 * 7 + 20} width="45" height="5" />
+          </ContentLoader>
+        );
+      }}
+    />
+  );
+};
+
+export const BarangShimmer = props => {
+  const {portrait} = props;
+
+  let itemWidth = windowWidth / 5 - 2;
+  let itemHeight = itemWidth * 2 - 5;
+
+  let pictureWidth = windowWidth / 6 - 6;
+  let pictureHeight = pictureWidth;
+  return (
+    <FlatList
+      showsHorizontalScrollIndicator={false}
+      data={[1, 2, 3, 4, 5, 6,7,8,9]}
+      keyExtractor={(item, index) => {
+        return `${index}`;
+      }}
+      style={{}}
+      numColumns={3}
+      contentContainerStyle={{}}
       renderItem={({item, index}) => {
         return (
           <ContentLoader height={itemHeight} width={itemWidth}>
