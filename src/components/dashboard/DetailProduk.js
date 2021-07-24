3db33abe54a79856292bcf0ddd8fa6b72f5f9606
NovandiKevin <kevinpret27@gmail.com>
2021-07-24 17:03:17 +0700

    Tambah Keranjang

--- src/components/dashboard/DetailProduk.js
+++ src/components/dashboard/DetailProduk.js
@@ -35,7 +35,8 @@ export default class DetailProduk extends Component {
     };
   }
 
-  showAlert = () =>
+  showAlert = () => {
+    AsyncStorage.setItem('refreshKeranjang', 'IYA');
     Alert.alert(
       'Berhasil',
       'Berhasil dimasukkan keranjang',
@@ -47,7 +48,9 @@ export default class DetailProduk extends Component {
         },
         {
           text: 'Bayar',
-          onPress: () => {},
+          onPress: () => {
+            this.props.navigation.navigate('Keranjang');
+          },
           style: 'default',
         },
       ],
@@ -55,6 +58,7 @@ export default class DetailProduk extends Component {
         cancelable: true,
       },
     );
+  };
 
   tambahKeranjang = async () => {
     if (!this.state.varian && this.state.dataProduk.varian.length > 0) {
@@ -97,7 +101,7 @@ export default class DetailProduk extends Component {
   listProdukTerkait = dataProduk => {
     const urlGambar = `${BASE_URL()}/image/barang/`;
 
-    const imgWidth = (Dimensions.get('screen').width * 0.85) / 2;
+    const imgWidth = (Dimensions.get('screen').width * 0.88) / 2;
     return (
       <Box py={3} mt={2} px={3} bg="white">
         <Text mt={2} bold={true}>
@@ -176,133 +180,142 @@ export default class DetailProduk extends Component {
           {/* <Box>
             <Text>Ini Header Gan</Text>
           </Box> */}
-          <ScrollView px={2} flex={1} nestedScrollEnabled>
-            <Resource
-              url={`${BASE_URL()}/barang/${this.props.route.params.idproduk}`}>
-              {({loading, error, payload: data, refetch}) => {
-                if (loading) {
-                  return (
-                    <Box flex={1}>
-                      <Spinner color="green" />
-                    </Box>
-                  );
-                }
-
-                if (this.state.dataProduk.length == 0) {
-                  this.setState({dataProduk: data.data});
-                }
-                return (
-                  <>
-                    <Box
-                      bg="white"
-                      height={Dimensions.get('screen').height * 0.25}>
-                      <ImageLoad
-                        flex={1}
-                        style={{
-                          resizeMode: 'contain',
-                        }}
-                        url={
-                          urlGambar +
-                          data.data.foto_barang +
-                          '?date=' +
-                          this.state.refresh
-                        }
-                        alt={data.data.nama}
-                      />
-                    </Box>
-                    <Box bg="white" mt={2} py={4} px={3}>
-                      <Text bold fontSize={20}>
-                        Rp.{toCurrency(data.data.harga)}
-                      </Text>
-                      <Text fontSize={14}>{data.data.nama}</Text>
-                    </Box>
-                    <Pressable
-                      onPress={() => {
-                        this.props.navigation.navigate('DetailToko', {
-                          idtoko: data.data.id_merchant,
-                        });
-                      }}>
-                      <Box bg="white" mt={2} py={4} px={3}>
-                        <HStack alignItems="center">
-                          <Avatar
-                            mr={4}
-                            source={{
-                              uri: urlToko + data.data.foto_merchant,
-                            }}
-                            alt={data.data.nama_toko}
-                          />
-                          <Text bold={true}>{data.data.nama_toko}</Text>
-                        </HStack>
+          <FlatList
+            flex={1}
+            keyExtractor={item => item}
+            data={[1]}
+            renderItem={() => (
+              <Resource
+                url={`${BASE_URL()}/barang/${
+                  this.props.route.params.idproduk
+                }`}>
+                {({loading, error, payload: data, refetch}) => {
+                  if (loading) {
+                    return (
+                      <Box pt={10}>
+                        <Spinner mt={20} color="green" />
                       </Box>
-                    </Pressable>
+                    );
+                  }
 
-                    {data.data.varian?.length > 0 && (
-                      <Box bg="white" mt={2} py={4} px={3}>
-                        <Text bold={true}>Pilih Variasi</Text>
-                        <FlatList
-                          mt={2}
-                          data={data.data.varian}
-                          numColumns={4}
-                          keyExtractor={(item, index) => index}
-                          renderItem={({item}) => {
-                            return (
-                              <Pressable
-                                onPress={() => {
-                                  this.setState({varian: item.id});
-                                }}>
-                                <Box
-                                  py={3}
-                                  px={4}
-                                  rounded={10}
-                                  bgColor={
-                                    item.id == this.state.varian
-                                      ? '#007bff'
-                                      : 'grey'
-                                  }
-                                  mx={1}
-                                  my={1}>
-                                  <Text fontSize={13} color="white">
-                                    {item.nama_varian}
-                                  </Text>
-                                </Box>
-                              </Pressable>
-                            );
+                  if (this.state.dataProduk != data.data) {
+                    this.setState({dataProduk: data.data});
+                  }
+                  return (
+                    <>
+                      <Box
+                        bg="white"
+                        height={Dimensions.get('screen').height * 0.25}>
+                        <ImageLoad
+                          flex={1}
+                          style={{
+                            resizeMode: 'contain',
                           }}
+                          url={
+                            urlGambar +
+                            data.data.foto_barang +
+                            '?date=' +
+                            this.state.refresh
+                          }
+                          alt={data.data.nama}
                         />
                       </Box>
-                    )}
-                    <Box bg="white" mt={2} py={4} px={3} pb={8}>
-                      <Text bold={true}>Detail Produk</Text>
-                      <HStack space={1} mt={4} mb={2}>
-                        <Text fontSize={14} flex={1}>
-                          Berat :
+                      <Box bg="white" mt={2} py={4} px={3}>
+                        <Text bold fontSize={20}>
+                          Rp.{toCurrency(data.data.harga)}
                         </Text>
-                        <Text fontSize={14} flex={1}>
-                          {data.data.berat} gram
-                        </Text>
-                      </HStack>
-                      <Divider />
-                      <HStack space={1} mt={2} mb={2}>
-                        <Text fontSize={14} flex={1}>
-                          Tersedia :
-                        </Text>
-                        <Text fontSize={14} flex={1}>
-                          {data.data.stok}
-                        </Text>
-                      </HStack>
-                      <Divider />
-                      <Text mt={4}>{data.data.deskripsi}</Text>
-                    </Box>
+                        <Text fontSize={14}>{data.data.nama}</Text>
+                      </Box>
+                      <Pressable
+                        onPress={() => {
+                          this.props.navigation.navigate('DetailToko', {
+                            idtoko: data.data.id_merchant,
+                          });
+                        }}>
+                        <Box bg="white" mt={2} py={4} px={3}>
+                          <HStack alignItems="center">
+                            <Avatar
+                              mr={4}
+                              source={{
+                                uri: urlToko + data.data.foto_merchant,
+                              }}
+                              alt={data.data.nama_toko}
+                            />
+                            <Text bold={true}>{data.data.nama_toko}</Text>
+                          </HStack>
+                        </Box>
+                      </Pressable>
 
-                    {data.data?.terkait?.length > 0 &&
-                      this.listProdukTerkait(data.data?.terkait)}
-                  </>
-                );
-              }}
-            </Resource>
-          </ScrollView>
-          <Box px={3} pb={2}>
+                      {data.data.varian?.length > 0 && (
+                        <Box bg="white" mt={2} py={4} px={3}>
+                          <Text bold={true}>Pilih Variasi</Text>
+                          <FlatList
+                            mt={2}
+                            data={data.data.varian}
+                            numColumns={4}
+                            keyExtractor={(item, index) => index}
+                            renderItem={({item}) => {
+                              return (
+                                <Pressable
+                                  onPress={() => {
+                                    this.setState({varian: item.id});
+                                  }}>
+                                  <Box
+                                    py={3}
+                                    px={4}
+                                    rounded={10}
+                                    bgColor={
+                                      item.id == this.state.varian
+                                        ? '#007bff'
+                                        : 'grey'
+                                    }
+                                    mx={1}
+                                    my={1}>
+                                    <Text fontSize={13} color="white">
+                                      {item.nama_varian}
+                                    </Text>
+                                  </Box>
+                                </Pressable>
+                              );
+                            }}
+                          />
+                        </Box>
+                      )}
+                      <Box bg="white" mt={2} py={4} px={3} pb={8}>
+                        <Text bold={true}>Detail Produk</Text>
+                        <HStack space={1} mt={4} mb={2}>
+                          <Text fontSize={14} flex={1}>
+                            Berat :
+                          </Text>
+                          <Text fontSize={14} flex={1}>
+                            {data.data.berat} gram
+                          </Text>
+                        </HStack>
+                        <Divider />
+                        <HStack space={1} mt={2} mb={2}>
+                          <Text fontSize={14} flex={1}>
+                            Tersedia :
+                          </Text>
+                          <Text fontSize={14} flex={1}>
+                            {data.data.stok}
+                          </Text>
+                        </HStack>
+                        <Divider />
+                        <Text mt={4}>{data.data.deskripsi}</Text>
+                      </Box>
+
+                      {data.data?.terkait?.length > 0 &&
+                        this.listProdukTerkait(data.data?.terkait)}
+                    </>
+                  );
+                }}
+              </Resource>
+            )}
+          />
+          <Box bg="white">
             <Button
+              mx={3}
+              mb={3}
               isLoading={this.state.loading}
               isLoadingText="Proses"
               onPress={() => this.tambahKeranjang()}
