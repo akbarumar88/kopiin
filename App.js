4351820b97336053a75cf90321a268091b550976
NovandiKevin <kevinpret27@gmail.com>
2021-07-25 18:17:42 +0700

    Perbaikan konflik

--- App.js
+++ App.js
@@ -7,49 +7,18 @@
  */
 import "react-native-gesture-handler"
 import React, { Component } from "react"
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from "react-native"
+import { StyleSheet } from "react-native"
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from "react-native/Libraries/NewAppScreen"
 import { NavigationContainer } from "@react-navigation/native"
 import { createStackNavigator } from "@react-navigation/stack"
 import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs"
 
 import Splash from "./src/components/universal/Splash"
-import {
-  NativeBaseProvider,
-  Button,
-  Input,
-  FormControl,
-  Icon,
-} from "native-base"
-import Ionicons from "react-native-vector-icons/Ionicons"
-import FontAwesome5 from "react-native-vector-icons/FontAwesome5"
-import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons"
+import { NativeBaseProvider } from "native-base"
 
-import Home from "./src/components/dashboard/Home"
-import Feed from "./src/components/dashboard/Feed"
-import Keranjang from "./src/components/dashboard/Keranjang"
-import Akun from "./src/components/dashboard/Akun"
-import Login from "./src/components/dashboard/Login"
 import Register from "./src/components/dashboard/Register"
 import Profil from "./src/components/akun/Profil"
 import Dashboard from "./src/components/dashboard/Index"
-import { theme } from "./src/utilitas/Config"
-import AsyncStorage from "@react-native-community/async-storage"
 
 import PilihLokasi from "./src/components/akun/PilihLokasi"
 import Shop from "./src/components/dashboard/Shop"
@@ -66,45 +35,8 @@ import DetailProduk from "./src/components/dashboard/DetailProduk"
 import DetailToko from "./src/components/dashboard/DetailToko"
 import UbahKeranjang from "./src/components/dashboard/UbahKeranjang"
 import Posting from "./src/components/akun/Posting"
-
 import PembayaranStack from "./src/components/pembayaran/PembayaranStack"
-
-const Stack = createStackNavigator()
-const Tab = createMaterialBottomTabNavigator()
-
-import Ionicons from "react-native-vector-icons/Ionicons"
-import FontAwesome5 from "react-native-vector-icons/FontAwesome5"
-import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons"
-
-import Home from "./src/components/dashboard/Home"
-import Feed from "./src/components/dashboard/Feed"
-import Keranjang from "./src/components/dashboard/Keranjang"
-import Akun from "./src/components/dashboard/Akun"
-import Login from "./src/components/dashboard/Login"
-import Register from "./src/components/dashboard/Register"
-import Profil from "./src/components/akun/Profil"
-import Dashboard from "./src/components/dashboard/Index"
-import { theme } from "./src/utilitas/Config"
-import AsyncStorage from "@react-native-community/async-storage"
-
-import PilihLokasi from "./src/components/akun/PilihLokasi"
-import Shop from "./src/components/dashboard/Shop"
-import FormProduk from "./src/components/akun/FormProduk"
-import MyProduk from "./src/components/akun/MyProduk"
-import UbahPasssword from "./src/components/akun/UbahPassword"
-
-import FormAlamat from "./src/components/akun/FormAlamat"
-import Alamat from "./src/components/akun/Alamat"
 import PilihAlamat from "./src/components/order/PilihAlamat"
-
-import HasilPencarian from "./src/components/dashboard/HasilPencarian"
-import Pencarian from "./src/components/dashboard/Pencarian"
-import DetailProduk from "./src/components/dashboard/DetailProduk"
-import DetailToko from "./src/components/dashboard/DetailToko"
-import UbahKeranjang from "./src/components/dashboard/UbahKeranjang"
-
-import PembayaranStack from "./src/components/pembayaran/PembayaranStack"
-
 const Stack = createStackNavigator()
 const Tab = createMaterialBottomTabNavigator()
 
@@ -196,7 +128,6 @@ class App extends Component {
             <Stack.Screen name="Alamat" component={Alamat} />
             <Stack.Screen name="Posting" component={Posting} />
 
-            <Stack.Screen name="Alamat" component={Alamat} />
             <Stack.Screen name="PilihAlamat" component={PilihAlamat} />
             <Stack.Screen name="PembayaranStack" component={PembayaranStack} />
           </Stack.Navigator>
