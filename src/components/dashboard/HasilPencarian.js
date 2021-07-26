6a44b5027d15aa0bdcd137d271fe2435b932ee58
NovandiKevin <kevinpret27@gmail.com>
2021-07-27 06:59:39 +0700

    Sorting dan Filter Toko

--- src/components/dashboard/HasilPencarian.js
+++ src/components/dashboard/HasilPencarian.js
@@ -16,6 +16,7 @@ import {
   Button,
   useDisclose,
   Actionsheet,
+  IconButton,
 } from 'native-base';
 import {toCurrency} from '../../utilitas/Function';
 import Resource from './../universal/Resource';
@@ -24,8 +25,9 @@ import {TabView, TabBar, SceneMap} from 'react-native-tab-view';
 import FooterLoading from '../universal/FooterLoading';
 import ImageLoad from './../universal/ImageLoad';
 import EmptyCart from '../universal/EmptyCart';
+import AsyncStorage from '@react-native-community/async-storage';
 
-export function ActionStatus({sorting}) {
+export function FilterProduk({sorting}) {
   const {isOpen, onOpen, onClose} = useDisclose();
   const [filterRating, setfilterRating] = useState(false);
   const [filterKategori, setfilterKategori] = useState('');
@@ -55,7 +57,18 @@ export function ActionStatus({sorting}) {
   }
   return (
     <>
-      <Button rounded={0} mb={1} onPress={onOpen}>
+      <Button
+        startIcon={
+          <Icon
+            as={<MaterialCommunityIcons name="sort" />}
+            color="white"
+            size="sm"
+          />
+        }
+        size="sm"
+        rounded={0}
+        mb={1}
+        onPress={onOpen}>
         Filter
       </Button>
 
@@ -63,7 +76,7 @@ export function ActionStatus({sorting}) {
         isOpen={isOpen}
         onClose={() => {
           onClose();
-          refresh();
+          // refresh();
         }}>
         <Actionsheet.Content pb={4} alignItems="flex-start">
           <Text bold mb={2} mx={3}>
@@ -158,6 +171,109 @@ export function ActionStatus({sorting}) {
     </>
   );
 }
+
+export function FilterToko({sorting}) {
+  const {isOpen, onOpen, onClose} = useDisclose();
+  const [sortingToko, setSortingToko] = useState('');
+  const [filterJenisToko, setfilterJenisToko] = useState('');
+
+  function refresh() {
+    sorting(sortingToko, filterJenisToko);
+  }
+  return (
+    <>
+      <Button
+        startIcon={
+          <Icon
+            as={<MaterialCommunityIcons name="sort" />}
+            color="white"
+            size="sm"
+          />
+        }
+        size="sm"
+        rounded={0}
+        mb={1}
+        onPress={onOpen}>
+        Filter
+      </Button>
+
+      <Actionsheet
+        isOpen={isOpen}
+        onClose={() => {
+          onClose();
+          // refresh();
+        }}>
+        <Actionsheet.Content pb={4} alignItems="flex-start">
+          <Text bold mb={2} mx={3}>
+            Urutkan
+          </Text>
+          <Button
+            variant={sortingToko == 'distance' ? 'solid' : 'outline'}
+            onPress={() => {
+              if (sortingToko == 'distance') {
+                setSortingToko('nama_toko');
+              } else {
+                setSortingToko('distance');
+              }
+            }}
+            size="sm">
+            Lokasi Terdekat
+          </Button>
+          <Divider mt={4} mb={2} />
+
+          <Resource url={`${BASE_URL()}/jenis`}>
+            {({loading, error, payload: data, refetch, fetchMore}) => {
+              if (loading) {
+                return <FooterLoading />;
+              }
+              return (
+                <>
+                  <Text bold mx={3} mb={2}>
+                    Jenis Toko
+                  </Text>
+                  <FlatList
+                    horizontal={true}
+                    data={data.data}
+                    renderItem={({item}) => (
+                      <Button
+                        mb={3}
+                        mr={2}
+                        onPress={() => {
+                          if (filterJenisToko == item.id) {
+                            setfilterJenisToko('');
+                          } else {
+                            setfilterJenisToko(item.id);
+                          }
+                        }}
+                        variant={
+                          filterJenisToko == item.id ? 'solid' : 'outline'
+                        }
+                        size="sm">
+                        {item.jenis}
+                      </Button>
+                    )}
+                  />
+                </>
+              );
+            }}
+          </Resource>
+          <HStack mt={2}>
+            <Button
+              onPress={() => {
+                onClose();
+                refresh();
+              }}
+              size="sm"
+              flex={1}
+              colorScheme="success">
+              Terapkan
+            </Button>
+          </HStack>
+        </Actionsheet.Content>
+      </Actionsheet>
+    </>
+  );
+}
 export default class HasilPencarian extends Component {
   defaultStoreAvatar =
     'https://cdn.icon-icons.com/icons2/1706/PNG/512/3986701-online-shop-store-store-icon_112278.png';
@@ -166,7 +282,8 @@ export default class HasilPencarian extends Component {
 
   constructor(props) {
     super(props);
-    this.setSorting = this.setSorting.bind(this);
+    this.setSortingProduk = this.setSortingProduk.bind(this);
+    this.setSortingToko = this.setSortingToko.bind(this);
     this.state = {
       cari: this.props.route.params.cari,
       pencarian: this.props.route.params.cari,
@@ -175,26 +292,42 @@ export default class HasilPencarian extends Component {
         {key: 'barang', title: 'Barang'},
         {key: 'toko', title: 'Toko'},
       ],
+      lat: 10,
+      long: 10,
+      initialLoading: true,
       limit: 10,
       hasMoreProduct: true,
       hasMoreShop: true,
-      sorting: 'created_at',
+      sortProduk: 'created_at',
       orderby: 'DESC',
       rating: 0,
-      kategori: null,
+      filterKategori: null,
+      filterJenistoko: null,
+      sortToko: 'nama_toko',
     };
   }
 
-  setSorting(sorting, orderby = 'ASC', rating = false, kategori = null) {
+  async componentDidMount() {
+    let lat = await AsyncStorage.getItem('lat');
+    let long = await AsyncStorage.getItem('long');
+    this.setState({lat, long, initialLoading: false});
+  }
+
+  setSortingProduk(
+    sorting,
+    orderby = 'ASC',
+    rating = false,
+    filterKategori = null,
+  ) {
     let minimalRating = 0;
     if (rating) {
       minimalRating = 4;
     }
     this.setState({
-      sorting: sorting,
+      sortProduk: sorting,
       orderby: orderby,
       rating: minimalRating,
-      kategori: kategori,
+      filterKategori: filterKategori,
     });
   }
 
@@ -235,22 +368,23 @@ export default class HasilPencarian extends Component {
   }
 
   listProduct = () => {
-    const {pencarian, limit, sorting, orderby, rating, kategori} = this.state;
+    const {pencarian, limit, sortProduk, orderby, rating, filterKategori} =
+      this.state;
     const urlGambar = `${BASE_URL()}/image/barang/`;
 
     const imgWidth = (Dimensions.get('screen').width * 0.5) / 2;
     return (
       <>
-        <ActionStatus sorting={this.setSorting} />
+        <FilterProduk sorting={this.setSortingProduk} />
         <Resource
           url={`${BASE_URL()}/barang`}
           params={{
             cari: pencarian,
             limit,
-            orderby: sorting,
+            orderby: sortProduk,
             sort: orderby,
             rating: rating,
-            kategori: kategori,
+            kategori: filterKategori,
           }}>
           {({loading, error, payload: data, refetch, fetchMore}) => {
             if (loading) {
@@ -358,116 +492,145 @@ export default class HasilPencarian extends Component {
     );
   };
 
+  setSortingToko(sortToko = 'nama_toko', filterJenisToko = null) {
+    this.setState({
+      sortToko: sortToko,
+      filterJenistoko: filterJenisToko,
+    });
+  }
+
   listShop = () => {
-    const {pencarian, limit} = this.state;
+    const {
+      pencarian,
+      limit,
+      sortToko,
+      filterJenistoko,
+      initialLoading,
+      lat,
+      long,
+    } = this.state;
     const urlGambar = `${BASE_URL()}/image/merchant/`;
 
     const imgWidth = (Dimensions.get('screen').width * 0.5) / 2;
     return (
-      <Resource
-        url={`${BASE_URL()}/shop`}
-        params={{
-          cari: pencarian,
-          limit,
-        }}>
-        {({loading, error, payload: data, refetch, fetchMore}) => {
-          if (loading) {
-            return (
-              <Box flex={1} justifyContent="center">
-                <FooterLoading full />
-              </Box>
-            );
-          }
-          if (!data.data.length) {
-            return (
-              <EmptyCart
-                title="Data tidak ditemukan"
-                description="Data yang anda cari tidak ditemukan, Coba cari kata kunci yang lain."
-                icon={
-                  <Icon
-                    as={MaterialCommunityIcons}
-                    name="file-search"
-                    size="lg"
-                    color="#555"
-                  />
-                }
-              />
-            );
-          }
-          let nextOffset = data.data.length;
-
-          return (
-            <FlatList
-              numColumns={2}
-              horizontal={false}
-              flex={1}
-              data={[...data.data]}
-              keyExtractor={(item, index) => item.id}
-              showsVerticalScrollIndicator={false}
-              renderItem={({item, index}) => (
-                <Pressable
-                  onPress={() => {
-                    this.props.navigation.navigate('DetailToko', {
-                      idtoko: item.id,
-                    });
-                  }}
-                  flex={0.5}>
-                  <Box
-                    bgColor="coolGray.100"
-                    mx={1}
-                    key={index}
-                    borderRadius={20}
-                    pb={4}
-                    mb={1}
-                    alignItems="center">
-                    <ImageLoad
-                      alignSelf="center"
-                      resizeMode="contain"
-                      mt={2}
-                      mb={2}
-                      style={[
-                        {width: imgWidth, height: imgWidth},
-                        {
-                          borderTopLeftRadius: 10,
-                          borderTopRightRadius: 10,
-                        },
-                      ]}
-                      onError={() => {}}
-                      url={urlGambar + item.foto_merchant + '?' + new Date()}
-                      alt={item.nama_toko}
-                    />
-                    <Text fontSize="sm" isTruncated>
-                      {item.nama_toko}
-                    </Text>
-                    <Text fontSize="xs" bold color="grey">
-                      {item.alamat_toko}
-                    </Text>
+      <>
+        <FilterToko sorting={this.setSortingToko} />
+        {!initialLoading && (
+          <Resource
+            url={`${BASE_URL()}/shop`}
+            params={{
+              jenis: filterJenistoko,
+              orderby: sortToko,
+              cari: pencarian,
+              lat: lat,
+              long: long,
+              limit,
+            }}>
+            {({loading, error, payload: data, refetch, fetchMore}) => {
+              if (loading) {
+                return (
+                  <Box flex={1} justifyContent="center">
+                    <FooterLoading full />
                   </Box>
-                </Pressable>
-              )}
-              refreshing={false}
-              onRefresh={() => {
-                this.setState({hasMoreShop: true});
-                refetch();
-              }}
-              onEndReached={() => {
-                fetchMore({offset: nextOffset}, (newPayload, oldPayload) => {
-                  if (newPayload.data.length < limit) {
-                    this.setState({hasMoreShop: false});
-                  }
-                  return {
-                    ...oldPayload,
-                    data: [...oldPayload.data, ...newPayload.data],
-                  };
-                });
-              }}
-              ListFooterComponent={
-                this.state.hasMoreShop ? <FooterLoading /> : null
+                );
               }
-            />
-          );
-        }}
-      </Resource>
+              if (!data.data.length) {
+                return (
+                  <EmptyCart
+                    title="Data tidak ditemukan"
+                    description="Data yang anda cari tidak ditemukan, Coba cari kata kunci yang lain."
+                    icon={
+                      <Icon
+                        as={MaterialCommunityIcons}
+                        name="file-search"
+                        size="lg"
+                        color="#555"
+                      />
+                    }
+                  />
+                );
+              }
+              let nextOffset = data.data.length;
+
+              return (
+                <FlatList
+                  numColumns={2}
+                  horizontal={false}
+                  flex={1}
+                  data={[...data.data]}
+                  keyExtractor={(item, index) => item.id}
+                  showsVerticalScrollIndicator={false}
+                  renderItem={({item, index}) => (
+                    <Pressable
+                      onPress={() => {
+                        this.props.navigation.navigate('DetailToko', {
+                          idtoko: item.id,
+                        });
+                      }}
+                      flex={0.5}>
+                      <Box
+                        bgColor="coolGray.100"
+                        mx={1}
+                        key={index}
+                        borderRadius={20}
+                        pb={4}
+                        mb={1}
+                        alignItems="center">
+                        <ImageLoad
+                          alignSelf="center"
+                          resizeMode="contain"
+                          mt={2}
+                          mb={2}
+                          style={[
+                            {width: imgWidth, height: imgWidth},
+                            {
+                              borderTopLeftRadius: 10,
+                              borderTopRightRadius: 10,
+                            },
+                          ]}
+                          onError={() => {}}
+                          url={
+                            urlGambar + item.foto_merchant + '?' + new Date()
+                          }
+                          alt={item.nama_toko}
+                        />
+                        <Text fontSize="sm" isTruncated>
+                          {item.nama_toko}
+                        </Text>
+                        <Text fontSize="xs" bold color="grey">
+                          {item.alamat_toko}
+                        </Text>
+                      </Box>
+                    </Pressable>
+                  )}
+                  refreshing={false}
+                  onRefresh={() => {
+                    this.setState({hasMoreShop: true});
+                    refetch();
+                  }}
+                  onEndReached={() => {
+                    fetchMore(
+                      {offset: nextOffset},
+                      (newPayload, oldPayload) => {
+                        if (newPayload.data.length < limit) {
+                          this.setState({hasMoreShop: false});
+                        }
+                        return {
+                          ...oldPayload,
+                          data: [...oldPayload.data, ...newPayload.data],
+                        };
+                      },
+                    );
+                  }}
+                  ListFooterComponent={
+                    this.state.hasMoreShop ? <FooterLoading /> : null
+                  }
+                />
+              );
+            }}
+          </Resource>
+        )}
+      </>
     );
   };
 
